cd97976fae0428146c6d98c63c8270f1207a54ed
Михаил <rotari733@gmail.com>
2022-08-28 19:09:39 +0300

    init

---
+++ src/main/java/org/example/App.java
@@ -0,0 +1,23 @@
+package org.example;
+
+import io.grpc.Server;
+import io.grpc.ServerBuilder;
+
+import java.io.IOException;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args ) throws IOException, InterruptedException {
+        Server server = ServerBuilder.forPort(8080)
+                .addService(new RotarImpl())
+                //.addService(new RotarImpl())
+                .build();
+        server.start();
+        System.out.println("server Started");
+        server.awaitTermination();
+    }
+}
---
+++ src/main/java/org/example/RotarImpl.java
@@ -0,0 +1,20 @@
+package org.example;
+
+import com.example.grpc.RotarGrpc;
+import com.example.grpc.RotarOuterClass;
+import io.grpc.stub.StreamObserver;
+
+public class RotarImpl extends RotarGrpc.RotarImplBase {
+    @Override
+    public void greeting(RotarOuterClass.HelloRequest request,
+                         StreamObserver<RotarOuterClass.HelloResponse> responseObserver) {
+        System.out.println(request);
+        RotarOuterClass.HelloResponse response = RotarOuterClass.HelloResponse
+                .newBuilder().setGreting("hello from server " + request.getName())
+                .build();
+        responseObserver.onNext(response);
+        responseObserver.onCompleted();
+
+
+    }
+}
